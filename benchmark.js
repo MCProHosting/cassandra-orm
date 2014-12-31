61435ac4b8a5baf9e0640670f6b5e1d2399eea97
Connor Peet <connor@peet.io>
2014-12-30 22:41:15 -0600

    Benchmarking, performance improvements

---
+++ benchmark.js
@@ -0,0 +1,46 @@
+// To use, `npm install benchmark microtime`
+
+var builders = require('./lib/cql/builders');
+var t = require('./lib/cql/types');
+var Raw = require('./lib/cql/stmt/raw');
+
+new (require('benchmark').Suite)()
+    .add('Select statement builder', function () {
+        builders.select()
+            .columns('a', 'b')
+            .from('tbl')
+            .where(t.Text('a'), '<', 3)
+            .andWhere('c', '>', 2)
+            .parameterize();
+    })
+    .add('Delete statement builder', function () {
+        builders.delete()
+            .columns('a', 'b')
+            .from('tbl')
+            .where(t.Text('a'), '<', 3)
+            .andWhere('c', '>', 2)
+            .parameterize();
+    })
+    .add('Insert statement builder', function () {
+        builders.insert()
+            .data({ 'a': 'b' })
+            .into('tbl')
+            .ifNotExists()
+            .parameterize();
+    })
+    .add('Update statement builder', function () {
+        builders.update()
+            .table('tbl')
+            .where(t.Text('a'), '<', 3)
+            .andWhere('c', '>', 2)
+            .set('foo = \'bar\'')
+            .set('a', 2)
+            .set('a', 2, 3)
+            .timestamp(10)
+            .ttl(10)
+            .parameterize();
+    })
+    .on('cycle', function(event) {
+        console.log(String(event.target));
+    })
+    .run();
