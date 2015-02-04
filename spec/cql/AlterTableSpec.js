c5841d04099a94b9793d1ef9add4250e7514f91c
Connor Peet <connor@peet.io>
2015-02-04 14:55:51 -0500

    Alter table builder

---
+++ spec/cql/AlterTableSpec.js
@@ -0,0 +1,61 @@
+var AlterTable = require('../../lib/cql/queries/alterTable');
+var t  = require('../../lib/cql/types');
+
+describe('alter table', function () {
+    it('sets column type with strings', function () {
+        expect(new AlterTable()
+            .table('tbl')
+            .type('col', 'text')
+            .parameterize()
+        ).toEqual([[], 'ALTER TABLE tbl ALTER col TYPE text;']);
+    });
+    it('sets column type with column object', function () {
+        expect(new AlterTable()
+            .table('tbl')
+            .type(t.Text('col'))
+            .parameterize()
+        ).toEqual([[], 'ALTER TABLE tbl ALTER col TYPE text;']);
+    });
+    it('adds columns with strings', function () {
+        expect(new AlterTable()
+            .table('tbl')
+            .add('col', 'text')
+            .parameterize()
+        ).toEqual([[], 'ALTER TABLE tbl ADD col text;']);
+    });
+    it('adds columns with object', function () {
+        expect(new AlterTable()
+            .table('tbl')
+            .add(t.Text('col'))
+            .parameterize()
+        ).toEqual([[], 'ALTER TABLE tbl ADD col text;']);
+    });
+    it('drops columns', function () {
+        expect(new AlterTable()
+            .table('tbl')
+            .drop(t.Text('col'))
+            .parameterize()
+        ).toEqual([[], 'ALTER TABLE tbl DROP col;']);
+    });
+    it('renames columns', function () {
+        expect(new AlterTable()
+            .table('tbl')
+            .rename('col1', 'col2')
+            .parameterize()
+        ).toEqual([[], 'ALTER TABLE tbl RENAME col1 TO col2;']);
+    });
+    it('updates column properties single', function () {
+        expect(new AlterTable()
+            .table('tbl')
+            .setProperties({ comment: 'Hello World' })
+            .parameterize()
+        ).toEqual([[], 'ALTER TABLE tbl WITH comment = \'Hello World\';']);
+    });
+    it('updates column properties multiple', function () {
+        expect(new AlterTable()
+            .table('tbl')
+            .setProperties({ comment: 'Hello World', read_repair_chance: 0.2 })
+            .parameterize()
+        ).toEqual([[], 'ALTER TABLE tbl WITH comment = \'Hello World\' AND read_repair_chance = 0.2;']);
+    });
+});
